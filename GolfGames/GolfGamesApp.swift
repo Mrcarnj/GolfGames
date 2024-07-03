2789fb7c14efff46a3e916fd449aa897cbfc4ea5
Dieter <mrcarnj@gmail.com>
2024-07-03 10:56:54 -0600

    Initial Commit

---
+++ GolfGames/GolfGamesApp.swift
@@ -0,0 +1,17 @@
+//
+//  GolfGamesApp.swift
+//  GolfGames
+//
+//  Created by Mike Dietrich on 7/3/24.
+//
+
+import SwiftUI
+
+@main
+struct GolfGamesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
