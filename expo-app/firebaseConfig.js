3b9f3a0345e3a6396137d3a0645d4e43d6b2dda9
Dieter <mrcarnj@gmail.com>
2025-07-07 15:49:55 -0600

    starting migration from scratch

--- expo-app/firebaseConfig.js
+++
@@ -1,9 +0,0 @@
-// Firebase configuration for Expo app (migrated from iOS GoogleService-Info.plist)
-export const firebaseConfig = {
-  apiKey: "AIzaSyBBaqH9_FAhboeEoOih_FaMoiwpiAlN9kI",
-  authDomain: "gamblegolf-19624.firebaseapp.com",
-  projectId: "gamblegolf-19624",
-  storageBucket: "gamblegolf-19624.appspot.com",
-  messagingSenderId: "64996444063",
-  appId: "1:64996444063:web:20c8e1ca5d553a902ddfaa",
-};
--- expo-app/types/Golfer.ts
+++
@@ -1,7 +0,0 @@
-export interface Golfer {
-  id: string;
-  firstName: string;
-  lastName: string;
-  handicap: number;
-  // Add other fields as needed (e.g., ghinNumber)
-}
